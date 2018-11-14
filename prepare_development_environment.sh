85e113caa8d3344014f04601af90c0b4f2e878e6
Junki Ishikawa <69guitar1015@gmail.com>
2018-11-14 12:54:56 +0900

    Add prepare_development_environment.sh

---
+++ prepare_development_environment.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+git clone https://github.com/69guitar1015/anyenv ~/.anyenv
+echo 'export PATH="$HOME/.anyenv/bin:$PATH"' >> ~/.bash_profile
+echo 'eval "$(anyenv init -)"' >> ~/.bash_profile
+export PATH="$HOME/.anyenv/bin:$PATH"
+eval "$(anyenv init -)"
+
+anyenv install pyenv
+eval "$(anyenv init -)"
+
+pyenv install miniconda3-latest
+pyenv global miniconda3-latest
+
+echo "Preparation is finished"
+echo "Execute 'exec $SHELL -l' yourself"
