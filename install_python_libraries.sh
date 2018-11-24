ce4bf6aa082fb516787e19491621bffe77d70860
Junki Ishikawa <69guitar1015@gmail.com>
2018-11-24 20:22:08 +0900

    Add install_python_libraries.sh

---
+++ install_python_libraries.sh
@@ -0,0 +1,7 @@
+pip install -U pip
+conda update -y conda
+
+conda install -y \
+    cython numpy scipy scikit-learn matplotlib seaborn plotly \
+    pandas joblib ipython jupyter
+conda install -y -c conda-forge jupyter_contrib_nbextensions
