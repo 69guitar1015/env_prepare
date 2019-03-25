a45e713b221d067e58eb71435f2a69a91a759655
Junki Ishikawa <69guitar1015@gmail.com>
2019-03-25 10:50:05 +0900

    Update 02_install_python_libraries.sh

--- 02_install_python_libraries.sh
+++ 02_install_python_libraries.sh
@@ -5,3 +5,4 @@ conda install -y \
     cython numpy scipy scikit-learn matplotlib seaborn plotly \
     pandas joblib ipython jupyter
 conda install -y -c conda-forge jupyter_contrib_nbextensions
+jupyter notebook --generate-config
